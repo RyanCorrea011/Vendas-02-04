d035dd97d58172fa52b3277ab216f9e1dc0d5d30
RyanCorrea011 <127565119+RyanCorrea011@users.noreply.github.com>
2023-03-30 16:01:23 -0300

    Update Vendas.cs

--- ListaDeVendas/Vendas.cs
+++ ListaDeVendas/Vendas.cs
@@ -1,41 +1,34 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ListaDeVendas
 {
-    public class Vendas
+    public class Venda
     {
         public int Id { get; set; }
-        public string Data { get; set; }
+        public DateTime Data { get; set; }
+        public List<Produto> Produtos { get; set; }
 
-
-        public Vendas(int id, string data)
+        public Venda(int id, DateTime data, List<Produto> produtos)
         {
             Id = id;
             Data = data;
+            Produtos = produtos;
         }
 
-        public int GetId()
+        public void ImprimirVenda()
         {
-            return Id;
-        }
-
-        public void SetId(int id)
-        {
-            Id = id;
-        }
-
-        public string GetData()
-        {
-            return Data;
-        }
-
-        public void SetData(string data)
-        {
-            Data = data;
+            Console.WriteLine("Id da venda: " + Id);
+            Console.WriteLine("Data da venda: " + Data);
+            Console.WriteLine("Produtos comprados:");
+            foreach (Produto produto in Produtos)
+            {
+                Console.WriteLine("- " +"Id do Produto  "+  produto.IdP +  "\n" + produto.Nome + " R$" + produto.Valor );
+            }
         }
     }
 }
